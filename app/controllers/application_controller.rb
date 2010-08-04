ff2aa9ddb348a8d16347b2e17665e02c4bc75f96
Georg Apitz <georg@usebar.org>
2010-08-04 07:23:21 -0600

    added affirmative action corpus parser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-
+require 'hpricot'
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+  def readDoc
+    docname = "lib/data/2007-01-30-afaction-corpus.sgml"
+    docnumbers = Array.new
+    doc = open(docname) { |f| Hpricot(f) }
+    doc.search("doc") do |document|
+      #puts  "#{(document/"docno").first.inner_html}"
+      #puts "#{document.inner_html}"
+      docnumbers << (document/"docno").first.inner_html
+    end
+    return docnumbers
+  end
+  
 end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,5 @@
 class SiteController < ApplicationController 
   def index
+    @docnumbers = readDoc
   end
 end
